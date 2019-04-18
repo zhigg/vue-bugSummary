34461fb7c21e8049ce6afaf8519b15ac96b098a3
zhigg <object_z@163.com>
2019-04-18 23:39:56 +0800

    temp

---
+++ vue-bug/src/components/addTeacher.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="addTeacher">
+          <el-form ref="form" :model="form" label-width="90px">
+            <el-form-item label="姓名:">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="性别:">
+              <el-input v-model="form.sexCode"></el-input>
+            </el-form-item>
+            <el-form-item label="所在城市:">
+              <el-select v-model="form.city"></el-select>
+            </el-form-item>
+            <el-form-item label="兴趣爱好:">
+              <el-input v-model="form.hobby"></el-input>
+            </el-form-item>
+            <el-button @click="saveInfo">保存</el-button>
+          </el-form>
+          
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        name: '',
+        sexCode: '',
+        city: [],
+        hobby: ''
+      },
+      teacherData: []
+    }
+  },
+  methods: {
+    saveInfo () {
+      this.teacherData.push(this.form)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  .addTeacher {
+    background-color: #09131d;
+    overflow: hidden;
+    height: 100%;
+    .el-form {
+      width: 500px;
+      margin: 200px auto;
+      >.el-button {
+        margin-left: 215px;
+      }
+    }
+  }
+</style>
--- vue-bug/src/router/index.js
+++ vue-bug/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import AddTeacher from '@/components/addTeacher'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/addTeacher'
+    },
+    {
+      path: '/addTeacher',
+      name: 'addteacher',
+      component: AddTeacher
     }
   ]
 })
